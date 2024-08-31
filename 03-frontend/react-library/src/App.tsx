0ff3d7082eae43ec52aad8cf2044623c886a25b1
KomendaKacper <komenda@student.agh.edu.pl>
2024-08-31 22:29:49 +0200

    Basic setup

---
+++ 03-frontend/react-library/src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <nav className='navbar navbar-expand-lg navbar-dark main-color py-3'>
+      <div className='container-fluid'>
+        <span className='navbar-brand'>React Library</span>
+        <button className='navbar-toggler' type='button'
+        data-bs-toggle='collapse' data-bs-target='#navbarNavDropdown'
+        aria-controls='navbarNavDropdown' aria-expanded='false'
+        aria-label='Toggle Navigation'>
+          <span className='navbar-toggler-icon'></span>
+        </button>
+      </div>
+    </nav>
+  );
+}
+
+export default App;
