b93c2d8a9d7e62889774e5b93511d35ea219959c
KomendaKacper <komenda@student.agh.edu.pl>
2024-09-01 14:32:45 +0200

    Home Page

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -1,19 +1,15 @@
-import './App.css';
+import "./App.css";
+import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
+import { Footer } from "./layouts/NavbarAndFooter/Footer";
+import { HomePage } from "./layouts/HomePage/HomePage";
 
-function App() {
+export const App = () => {
   return (
-    <nav className='navbar navbar-expand-lg navbar-dark main-color py-3'>
-      <div className='container-fluid'>
-        <span className='navbar-brand'>React Library</span>
-        <button className='navbar-toggler' type='button'
-        data-bs-toggle='collapse' data-bs-target='#navbarNavDropdown'
-        aria-controls='navbarNavDropdown' aria-expanded='false'
-        aria-label='Toggle Navigation'>
-          <span className='navbar-toggler-icon'></span>
-        </button>
-      </div>
-    </nav>
+    <div>
+      <Navbar />
+      <HomePage />
+      <Footer />
+    </div>
   );
 }
 
-export default App;
---
+++ 03-frontend/react-library/src/layouts/HomePage/Components/ExploreTopBooks.tsx
@@ -0,0 +1,22 @@
+export const ExploreTopBooks = () => {
+  return (
+    <div className="p-5 mb-4 bg-dark header">
+      <div
+        className="container-fludi py-5 text-white
+            d-flex justify-content-center allign-items-center"
+      >
+        <div>
+          <h1 className="display-5 fw-bold">Explore Top Books</h1>
+          <p className="col-md-8 fs-4">Where would you to go next?</p>
+          <a
+            type="button"
+            className="btn main-color btn-lg text-white"
+            href="#"
+          >
+            Explore top Books
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
